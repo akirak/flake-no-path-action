8f863f2cbf4572e3bb1f265dea0a32301b78a554
Akira Komamura <akira.komamura@gmail.com>
2022-02-17 23:26:37 +0900

    style: Apply deno fmt

--- main.ts
+++ main.ts
@@ -1,67 +1,71 @@
-const isGH = !!Deno.env.get('GITHUB_ACTIONS')
+const isGH = !!Deno.env.get("GITHUB_ACTIONS");
 import { styles } from "https://deno.land/x/ansi_styles@1.0.0/mod.ts";
 
 function usage() {
-  console.log('Usage: main <file>...')
+  console.log("Usage: main <file>...");
 }
 
 if (Deno.args.length == 0) {
-  usage()
-  Deno.exit(1)
+  usage();
+  Deno.exit(1);
 }
 if (Deno.args[0] === "-h" || Deno.args[0] === "--help") {
-  usage()
-  Deno.exit()
+  usage();
+  Deno.exit();
 }
 
 type FlakeRef = {
-  type: string
-  lastModified?: number
-  narHash?: string
-  url?: string
-  owner?: string
-  repo?: string
-  rev?: string
-  ref?: string
-  path?: string
-}
+  type: string;
+  lastModified?: number;
+  narHash?: string;
+  url?: string;
+  owner?: string;
+  repo?: string;
+  rev?: string;
+  ref?: string;
+  path?: string;
+};
 
 type NodeData = {
-  flake?: boolean
-  inputs?: { [name: string]: string }
-  locked: FlakeRef
-  original: FlakeRef
-}
+  flake?: boolean;
+  inputs?: { [name: string]: string };
+  locked: FlakeRef;
+  original: FlakeRef;
+};
 
 async function checkFlakeLock(filepath: string): Promise<boolean> {
   const lock = JSON.parse(await Deno.readTextFile(filepath));
-  const pathNodes =
-    Object
-      .entries(lock.nodes)
-      .map(([name, data]) => ({ name, data } as { name: string; data: NodeData }))
-  // The root node does not have locked property
-      .filter(({ data }) => !!data.locked?.path && data.locked.path[0] === "/" && ! /^\/nix\/store\//.test(data.locked.path))
+  const pathNodes = Object
+    .entries(lock.nodes)
+    .map(([name, data]) => ({ name, data } as { name: string; data: NodeData }))
+    // The root node does not have locked property
+    .filter(({ data }) =>
+      !!data.locked?.path && data.locked.path[0] === "/" &&
+      !/^\/nix\/store\//.test(data.locked.path)
+    );
 
   for (const node of pathNodes) {
     console.error(`${styles.red.open}\
 ${styles.bold.open}ERROR:${styles.bold.close} \
 ${filepath}: \
 ${node.name} is locked to a path.\
-${styles.red.close}`)
+${styles.red.close}`);
 
     if (isGH) {
-      console.log(`::error file=${filepath}::${node.name} is locked to a path.`)
+      console.log(
+        `::error file=${filepath}::${node.name} is locked to a path.`,
+      );
     }
   }
 
-  return pathNodes.length > 0
+  return pathNodes.length > 0;
 }
 
-const files = Deno.args
-let error = false
+const files = Deno.args;
+let error = false;
 for (const file of files) {
-  error ||= await checkFlakeLock(file)
+  error ||= await checkFlakeLock(file);
 }
 if (error) {
-  Deno.exit(1)
+  Deno.exit(1);
 }
