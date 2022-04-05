cd43da3442e4ffce4a11ffc10bd22b07d9c1ba66
Akira Komamura <akira.komamura@gmail.com>
2022-04-06 06:57:41 +0900

    style(nix): Apply the formatter

--- flake.nix
+++ flake.nix
@@ -11,63 +11,61 @@
     inputs.flake-utils.follows = "flake-utils";
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    , pre-commit-hooks
-    }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    pre-commit-hooks,
+  }:
     {
       overlay = _final: prev: {
-        flake-no-path = prev.callPackage ./release.nix { };
+        flake-no-path = prev.callPackage ./release.nix {};
       };
-    } //
-    flake-utils.lib.eachDefaultSystem
-      (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ self.overlay ];
-        };
-      in
-      {
-        packages = flake-utils.lib.flattenTree {
-          inherit (pkgs) flake-no-path;
-        };
-        defaultPackage = pkgs.flake-no-path;
+    }
+    // flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [self.overlay];
+      };
+    in {
+      packages = flake-utils.lib.flattenTree {
+        inherit (pkgs) flake-no-path;
+      };
+      defaultPackage = pkgs.flake-no-path;
 
-        checks = {
-          pre-commit-check = pre-commit-hooks.lib.${system}.run {
-            src = ./.;
-            hooks = {
-              alejandra.enable = true;
-              statix.enable = true;
-              deno-fmt = {
-                enable = true;
-                name = "Reformat deno code";
-                entry = "${pkgs.deno}/bin/deno fmt";
-                files = "\\.(t|j)sx?$";
-                pass_filenames = true;
-              };
-              deno-lint = {
-                enable = true;
-                name = "Lint deno code";
-                entry = "${pkgs.deno}/bin/deno lint";
-                files = "\\.(t|j)sx?$";
-                pass_filenames = true;
-              };
-              flake-no-path = {
-                enable = true;
-                name = "Ensure that flake.lock does not contain a local path";
-                entry = "${pkgs.flake-no-path}/bin/flake-no-path";
-                files = "flake\.lock$";
-                pass_filenames = true;
-              };
+      checks = {
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            alejandra.enable = true;
+            statix.enable = true;
+            deno-fmt = {
+              enable = true;
+              name = "Reformat deno code";
+              entry = "${pkgs.deno}/bin/deno fmt";
+              files = "\\.(t|j)sx?$";
+              pass_filenames = true;
+            };
+            deno-lint = {
+              enable = true;
+              name = "Lint deno code";
+              entry = "${pkgs.deno}/bin/deno lint";
+              files = "\\.(t|j)sx?$";
+              pass_filenames = true;
+            };
+            flake-no-path = {
+              enable = true;
+              name = "Ensure that flake.lock does not contain a local path";
+              entry = "${pkgs.flake-no-path}/bin/flake-no-path";
+              files = "flake\.lock$";
+              pass_filenames = true;
             };
           };
         };
-        devShell = pkgs.mkShell {
-          inherit (self.checks.${system}.pre-commit-check) shellHook;
-        };
-      });
+      };
+      devShell = pkgs.mkShell {
+        inherit (self.checks.${system}.pre-commit-check) shellHook;
+      };
+    });
 }
